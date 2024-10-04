368f574bb53aa7898361948e864b21b3eaead503
Никита Терентьев <mykytaterentievua@gmail.com>
2024-10-04 11:16:53 +0200

    layout fix brands

--- components/Brands.vue
+++ components/Brands.vue
@@ -2,7 +2,10 @@
     <section class="relative py-16 bg-gray-100 text-black">
       <!-- Content -->
       <div class="relative z-10 flex flex-col items-center">
-        <h2 class="text-4xl font-bold mb-12">Бренди з якими ми працюємо</h2>
+        <!-- Centered Heading with Rounded Background -->
+        <h2 class="text-3xl md:text-4xl font-bold mb-12 text-center px-4 py-2">
+          Бренди з якими ми працюємо
+        </h2>
   
         <div class="flex flex-col md:flex-row justify-center items-center space-y-6 md:space-y-0 md:space-x-6">
           <!-- Brand 1 -->
@@ -17,7 +20,7 @@
   
           <!-- Brand 3 -->
           <div class="flex justify-center">
-            <img src="/images/wix_logo.png" alt="Wix Logo" class="h-24">
+            <img src="/images/wix.png" alt="Wix Logo" class="h-24">
           </div>
         </div>
       </div>
