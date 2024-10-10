339cef69e84b01a709b9605bf691469f820b72a0
Никита Терентьев <mykytaterentievua@gmail.com>
2024-10-10 16:21:16 +0200

    brands logo update

--- components/Brands.vue
+++ components/Brands.vue
@@ -1,37 +1,46 @@
 <template>
-    <section class="relative py-16 bg-gray-100 text-black">
-      <!-- Content -->
-      <div class="relative z-10 flex flex-col items-center">
-        <!-- Centered Heading with Rounded Background -->
-        <h2 class="text-3xl md:text-4xl font-bold mb-12 text-center px-4 py-2">
-          Бренди з якими ми працюємо
-        </h2>
-  
-        <div class="flex flex-col md:flex-row justify-center items-center space-y-6 md:space-y-0 md:space-x-6">
-          <!-- Brand 1 -->
-          <div class="flex justify-center">
-            <img src="/images/mahle.png" alt="Mahle Logo" class="h-24">
-          </div>
-  
-          <!-- Brand 2 -->
-          <div class="flex justify-center">
-            <img src="/images/motul.png" alt="Motul Logo" class="h-24">
-          </div>
-  
-          <!-- Brand 3 -->
-          <div class="flex justify-center">
-            <img src="/images/wix_logo.png" alt="Wix Logo" class="h-24">
-          </div>
+  <section class="relative py-16 bg-gray-100 text-black">
+    <!-- Content -->
+    <div class="relative z-10 flex flex-col items-center">
+      <!-- Centered Heading with Rounded Background -->
+      <h2 class="text-3xl md:text-4xl font-bold mb-12 text-center px-4 py-2">
+        Наші партнери-виробники
+      </h2>
+
+      <div class="flex flex-col md:flex-row justify-center items-center space-y-6 md:space-y-0 md:space-x-6">
+        <!-- Brand 1 -->
+        <div class="flex justify-center">
+          <img src="/images/mahle.png" alt="Mahle Logo" class="h-24">
+        </div>
+
+        <!-- Brand 2 -->
+        <div class="flex justify-center">
+          <img src="/images/motul.png" alt="Motul Logo" class="h-24">
+        </div>
+
+        <!-- Brand 3 -->
+        <div class="flex justify-center">
+          <img src="/images/wix_logo.png" alt="Wix Logo" class="h-24">
+        </div>
+
+        <!-- Brand 4 (KYB) -->
+        <div class="flex justify-center">
+          <img src="/images/kyb.png" alt="KYB Logo" class="h-24">
+        </div>
+
+        <!-- Brand 5 (Castrol) -->
+        <div class="flex justify-center">
+          <img src="/images/Castrol.png" alt="Castrol Logo" class="h-24">
         </div>
       </div>
-    </section>
-  </template>
-  
-  <script setup>
-  // Static content component, no logic required
-  </script>
-  
-  <style scoped>
-  /* Add styles as needed */
-  </style>
-  
+    </div>
+  </section>
+</template>
+
+<script setup>
+// Static content component, no logic required
+</script>
+
+<style scoped>
+/* Add styles as needed */
+</style>
