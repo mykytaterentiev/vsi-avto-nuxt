06e0b457aabba251277c98eb90dc58634a796494
Никита Терентьев <mykytaterentievua@gmail.com>
2024-10-15 20:25:55 +0200

    services fix

--- components/Services.vue
+++ components/Services.vue
@@ -8,20 +8,20 @@
     <div class="relative z-10 flex flex-col items-center">
       <h2 class="text-4xl text-black font-bold mb-12">Наші Послуги</h2>
 
-      <div class="flex flex-col md:flex-row justify-center items-center space-y-6 md:space-y-0 w-full">
+      <div class="flex flex-col md:flex-row justify-center items-center space-y-5 md:space-y-0 w-full">
         <!-- Service 1 -->
         <div class="bg-customColors-kolya text-white rounded-2xl p-6 shadow-md w-full md:w-1/3 h-32 flex items-center justify-center text-center mx-2">
-          <h3 class="text-xl font-semibold">Високоякісні автозапчастини для автомобілів</h3>
+          <h3 class="text-xl font-semibold">Точний підбір запчастин за VIN</h3>
         </div>
 
         <!-- Service 2 -->
         <div class="bg-customColors-kolya text-white rounded-2xl p-6 shadow-md w-full md:w-1/3 h-32 flex items-center justify-center text-center mx-2">
-          <h3 class="text-xl font-semibold">Швидка та надійна доставка</h3>
+          <h3 class="text-xl font-semibold">Безкоштовна консультація</h3>
         </div>
 
         <!-- Service 3 -->
         <div class="bg-customColors-kolya text-white rounded-2xl p-6 shadow-md w-full md:w-1/3 h-32 flex items-center justify-center text-center mx-2">
-          <h3 class="text-xl font-semibold">Відмінне обслуговування клієнтів</h3>
+          <h3 class="text-xl font-semibold">Обмін та Повернення</h3>
         </div>
       </div>
     </div>
